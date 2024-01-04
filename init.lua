5fd87b10a9c5de880cd0f0b260edb06a4f44c12f
Aleksandr Vlasenko <77232221+ayuvlasenko@users.noreply.github.com>
2024-01-04 22:03:01 +0400

    feat: change theme, diagnostics

--- init.lua
+++ init.lua
@@ -18,11 +18,12 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "astrodark",
+  colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
-    virtual_text = true,
+    virtual_text = false,
     underline = true,
   },
 
